cb75b920c0f2d3442a88316961c1c49e14076202
Adam Bailey <ajbailey564@gmail.com>
2024-01-16 18:41:14 -0600

    use array of locations

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,27 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import MapView, { Marker } from 'react-native-maps';
 
 const Home = () => {
+  const [savedLocations, setSavedLocations] = useState([
+    {
+      id: 1,
+      latitude: 41.44935872697216,
+      longitude: -71.39942479096507,
+      title: 'Beavertail'
+    },
+    {
+      id: 2,
+      latitude: 41.430472940397664,
+      longitude: -71.45571682409623,
+      title: 'The Towers'
+    },
+    {
+      id: 3,
+      latitude: 41.57115960160157,
+      longitude: -71.18787139773296,
+      title: 'Groundswell'
+    }
+  ])
+
   const [mapRegion, setmapRegion] = useState({
     latitude: 41.44935872697216,
     longitude: -71.39942479096507,
@@ -39,7 +60,11 @@ const Home = () => {
         onPoiClick={(event) => {console.log('POI', event)}}
         // mapType='hybridFlyover'
       >
-        <Marker coordinate={mapRegion} title='Marker' />
+        {
+          savedLocations.map((location) => {
+            return (<Marker coordinate={location} title={location.title} key={location.id}/>);
+          })
+        }
       </MapView>
       <BottomSheet
         ref={bottomSheetRef}
